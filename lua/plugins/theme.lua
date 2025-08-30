b6ee5b8334ab568d9ef8798f59f5a6b443a1fed5
Oliver Lock <oliverlock@ntlworld.com>
2025-08-31 00:16:30 +0100

    Updated Theme for unused import

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -7,6 +7,12 @@ return {
       on_highlights = function(hl, colors)
         hl["@variable"] = { fg = "#f7768e" }
         hl["@type"] = { fg = "#e5c07b" }
+        hl["DiagnosticUnnecessary"] = {
+          fg = "#565f89",
+          italic = true,
+          strikethrough = true,
+          underline = true,
+        }
       end,
     },
   },
